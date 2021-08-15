70f38293f51cccb4026f6af0b9cd35d5333accff
Mervin <mervindiazlugo>
2021-08-14 22:14:34 -0300

    environment demo

--- src/features/environment.py
+++ src/features/environment.py
@@ -9,15 +9,17 @@ HourFormat = "%H%M%S"
 
 
 def before_all(self):
-    print('1- Before all')
+    # print('1- Before all')
+    pass
 
 
 def before_feature(self, feature):
-    print(f'2- Before Feature: {feature}')
+    # print(f'2- Before Feature: {feature}')
+    pass
 
 
 def before_scenario(self, scenario):
-    print(f'3- Before Scenario: {scenario}')
+    # print(f'3- Before Scenario: {scenario}')
     if Config.Environment == 'Dev':
         print("Hola Chicos Dev")
     if Config.Environment == 'Test':
@@ -25,25 +27,30 @@ def before_scenario(self, scenario):
 
 
 def before_step(self, step):
-    print(f'4- Before step {step}')
+    # print(f'4- Before step {step}')
+    pass
 
 
 def after_step(self, step):
-    print(f'4.1- after step {step}')
+    # print(f'4.1- after step {step}')
     pass
 
 
 def before_tag(self, tag):
-    if tag == "Test":
+    '''if tag == "Test":
         print("Hola, ejecutaste el tag test")
+'''
+    pass
 
 
 def after_all(self):
-    print('5- after all')
+    # print('5- after all')
+    pass
 
 
 def after_feature(self, feature):
-    print(f'6- after feature {feature}')
+    # print(f'6- after feature {feature}')
+    pass
 
 
 def after_scenario(self, scenario):
